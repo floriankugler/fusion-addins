8118d46d0b50e50f1ec3b9b42744c2016fa380d3
Florian Kugler <mail@floriankugler.com>
2026-01-15 15:46:13 +0100

    Improve curve healing by creating a new, unified curve instead of just joining disparate curves with coincident constraints

--- SharedUtils/utils/__init__.py
+++ SharedUtils/utils/__init__.py
@@ -3,7 +3,8 @@ from . import brep
 from . import misc
 from . import vector
 from . import matrix
+from . import sketch
 
-__all__ = ["fusion", "brep", "misc", "vector", "matrix"] 
+__all__ = ["fusion", "brep", "misc", "vector", "matrix", "sketch"] 
 
-misc.force_reload_modules('utils.fusion', 'utils.brep', 'utils.misc', 'utils.vector', 'utils.matrix')
+misc.force_reload_modules('utils.fusion', 'utils.brep', 'utils.misc', 'utils.vector', 'utils.matrix', 'utils.sketch')
